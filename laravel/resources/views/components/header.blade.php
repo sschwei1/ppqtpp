0ebf651bf0c033f8b09191ff4981e0881c9cd802
sschwei1 <24sebastian05@gmail.com>
2022-01-23 16:14:31 +0100

    fix some components

--- laravel/resources/views/components/header.blade.php
+++ laravel/resources/views/components/header.blade.php
@@ -1,8 +1,12 @@
 <div>
     <nav class="navbar shadow-lg">
 
-        <a href="{{$link}}"> <i class="navicon fa fa-chevron-left" aria-hidden="true"></i></a>
-        <div></div>
+        @if(!empty($link))
+            <a href="{{$link}}"> <i class="navicon fa fa-chevron-left" aria-hidden="true"></i></a>
+        @else
+            <div></div>
+        @endif
+
         <img style="width: 60px" src="{{URL::asset('img/logo_transparent.png')}}" alt="Logo"/>
 
     </nav>
